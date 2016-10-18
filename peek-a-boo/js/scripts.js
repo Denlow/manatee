b754eac670a45c9be3143d13ee7f171143f59f32
Dave Denlow and Joshua Rinard <joshua@>
2016-10-18 09:39:06 -0700

    this commits JS, 2 manatee pictures, bootstrap, jquery etc.

--- peek-a-boo/js/scripts.js
+++ peek-a-boo/js/scripts.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#manatee-showing").toggle();
-    $("#manatee-hidden").toggle();
+$(document).ready(function() {//after doc is ready, function runs.
+  $(".clickable1").click(function() {//click method runs a new function.
+    $("#manatee-showing").slideToggle();//manatee toggles
+    $("#manatee-hidden").slideToggle();
+
   });
-  $("p").click(function() {
-    $("img").show();
+
+  $("p").click(function() {//p tags clickable
+    $("img").show();// clickable shows image
+  });
+
+  $(".clickable2").click(function() { //.click method runs a new function on Supermanatee,
+    $("#supermanatee-showing").fadeToggle();//superManatee toggles
+    $("#supermanatee-hidden").fadeToggle();
   });
 });
