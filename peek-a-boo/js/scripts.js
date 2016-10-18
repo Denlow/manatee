6e19f1b86b6f76d3ff55357911ecad340d86fbf0
Dave Denlow and Joshua Rinard <dave@>
2016-10-18 11:13:40 -0700

    this adds updated JS, styles, pictures and text.

--- peek-a-boo/js/scripts.js
+++ peek-a-boo/js/scripts.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {//after doc is ready, function runs.
+
+
   $(".clickable1").click(function() {//click method runs a new function.
     $("#manatee-showing").slideToggle();//manatee toggles
     $("#manatee-hidden").slideToggle();
-
   });
 
   $("p").click(function() {//p tags clickable
     $("img").show();// clickable shows image
   });
+  $("h4").click(function() {//p tags clickable
+    $("h5").show();// clickable shows image
+  });
 
   $(".clickable2").click(function() { //.click method runs a new function on Supermanatee,
-    $("#supermanatee-showing").fadeToggle();//superManatee toggles
-    $("#supermanatee-hidden").fadeToggle();
+    $(".supermanatee-showing").fadeToggle();//superManatee toggles
+    $(".supermanatee-hidden").fadeToggle();
   });
+
+  $(".clickable3").click(function() { //.click method runs a new function on Supermanatee,
+    $(".content-showing").fadeToggle();//superManatee toggles
+    $(".content-hidden").fadeToggle();
+  });
+
+
 });
