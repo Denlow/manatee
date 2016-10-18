ca432bf730fc345092f31f2f78532f25f3a37a91
Dave Denlow and Joshua Rinard <dave@>
2016-10-18 13:32:25 -0700

    this will add further functions

--- peek-a-boo/js/scripts.js
+++ peek-a-boo/js/scripts.js
@@ -25,3 +25,39 @@ $(document).ready(function() {//after doc is ready, function runs.
 
 
 });
+
+//
+// $(document).ready(function() {//after doc is ready, function runs.
+//
+//
+//   $(".clickable1").click(function() {//click method runs a new function.
+//     $("#manatee-showing").slideToggle();//manatee toggles
+//     $("#manatee-hidden").slideToggle();
+//   });
+//
+//   $("p").click(function() {//p tags clickable
+//     $("img").show();// clickable shows image
+//   });
+//   $("h4").click(function() {//p tags clickable
+//     $("h5").show();// clickable shows image
+//   });
+//
+//   $(".clickable2").click(name() { //.click method runs a new function on Supermanatee,
+//     $(".supermanatee-showing").fadeToggle();//superManatee toggles, (method + function)
+//     $(".supermanatee-hidden").fadeToggle();
+//   });
+//
+//   $(".clickable3").click(name() { //.click method runs a new function on Supermanatee, method
+//
+//   });
+//
+//   function name() {
+//     $(".content-showing").fadeToggle();//superManatee toggles, function
+//     $(".content-hidden").fadeToggle();
+//   });
+//
+//
+
+
+
+});
